ee75ffcd3cf38474acdb3bf61b831c457e0728c8
Jhon Hernandez <jhobahego16@hotmail.com>
2023-12-07 20:37:32 -0500

    Add tipado a las variables de entorno

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_GIPHY_API_URL: string;
+  readonly PUBLIC_GIPHY_API_KEY: string;
+  readonly PUBLIC_TENOR_API_URL: string;
+  readonly PUBLIC_TENOR_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
